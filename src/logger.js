9deefbb0d03dccff5aa2dc521ad755d3958984fc
Gary Lai <garylaiph@gmail.com>
2016-01-08 17:31:23 +0800

    init commit

---
+++ src/logger.js
@@ -0,0 +1,9 @@
+'user strict';
+const winston = require('winston');
+
+module.exports = new (winston.Logger)({
+    level: 'debug',
+    transports: [
+        new (winston.transports.Console)()
+    ]
+});
