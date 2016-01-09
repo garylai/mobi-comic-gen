7ba0e086d6942763bd4890210ab246779a440a7f
Gary Lai <garylaiph@gmail.com>
2016-01-09 19:15:03 +0800

    setup cli dev

---
+++ bin/cli.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+'use strict';
+const commandLineArgs = require('command-line-args');
+const logger = require('../src/logger.js');
+
+const cli = commandLineArgs([
+    {name: 'target', alias: 't', type: String}
+]);
+
+let options = {};
+try {
+    options = cli.parse();
+} catch (e) {
+    options = {};
+}
+
+if(!options.target) {
+    process.stdout.write(cli.getUsage());
+    process.exit();
+} 
+
+logger.debug(JSON.stringify(options));
