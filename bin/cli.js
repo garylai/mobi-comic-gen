f2e875a13ca13a2c1d7956bb1b3058753f15bd41
Gary Lai <garylaiph@gmail.com>
2016-02-06 23:48:14 +0800

    implemented CLI

--- bin/cli.js
+++ bin/cli.js
@@ -3,9 +3,11 @@
 'use strict';
 const commandLineArgs = require('command-line-args');
 const logger = require('../lib/mcg_logger.js');
+const DM5Downloader = require('../app.js').DM5Downloader;
+const OPFBuilder = require('../app.js').OPFBuilder;
 
 const cli = commandLineArgs([
-    {name: 'target', alias: 't', type: String}
+    {name: 'target', alias: 't', type: String, description: 'target comic\'s id' }
 ]);
 
 let options = {};
@@ -16,8 +18,30 @@ try {
 }
 
 if(!options.target) {
-    process.stdout.write(cli.getUsage());
+    console.log(cli.getUsage());
     process.exit();
 } 
 
 logger.debug(JSON.stringify(options));
+
+const comicId = parseInt(options.target);
+
+const dm5Downloader = new DM5Downloader(options.target);
+
+dm5Downloader.getPageImagesAsync()
+    .then(function(mcgComic) {
+        const opfBuilder = new OPFBuilder(mcgComic.title, mcgComic.imagePaths);
+        opfBuilder.buildBook()
+            .then(function(pathToMobi) {
+                console.log('done');
+                console.log('generated comic located at:');
+                console.log(pathToMobi);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    })
+    .catch(function(err) {
+        console.log(err.stack);
+    });
+
