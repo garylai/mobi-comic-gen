0488b8e64195ff56e7085083cd5cd09ef4951492
Gary Lai <garylaiph@gmail.com>
2016-02-12 16:07:43 +0800

    updated downloader and opf builder so that they emit events instead of printing messages directly

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 "use strict";
 const DM5Downloader = require('./lib/mcg_dm5_comic_downloader.js');
 const OPFBuilder = require('./lib/mcg_opf_builder.js');
+const DM5BookLister = require('./lib/mcg_dm5_book_lister.js');
+const DM5DownloaderEvents = require('./lib/mcg_dm5_comic_downloader_events.js');
+const OPFBuilderEvents = require('./lib/mcg_opf_builder_events.js');
 
 exports.DM5Downloader = DM5Downloader;
 exports.OPFBuilder = OPFBuilder;
+exports.DM5BookLister = DM5BookLister;
+exports.DM5DownloaderEvents = DM5DownloaderEvents;
+exports.OPFBuilderEvents = OPFBuilderEvents;
--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,8 @@ const commandLineArgs = require('command-line-args');
 const logger = require('../lib/mcg_logger.js');
 const DM5Downloader = require('../app.js').DM5Downloader;
 const OPFBuilder = require('../app.js').OPFBuilder;
+const DM5DownloaderEvents = require('../app.js').DM5DownloaderEvents;
+const OPFBuilderEvents = require('../app.js').OPFBuilderEvents;
 const co = require('co');
 const fs = require('fs');
 
@@ -25,7 +27,21 @@ if(!options.target || options.imageQuality < 1 || options.imageQuality > 100) {
     process.exit();
 }
 
+let totalNumberOfPages = null;
 const dm5Downloader = new DM5Downloader(options.target);
+dm5Downloader.on(DM5DownloaderEvents.Title, function(title) {
+    process.stdout.write('Title: ' + title + '\n');
+});
+dm5Downloader.on(DM5DownloaderEvents.TotalNumberOfPage, function(total) {
+    totalNumberOfPages = total;
+    process.stdout.write('Total number of pages: ' + totalNumberOfPages + '\n');
+});
+dm5Downloader.on(DM5DownloaderEvents.Downloaded, function(downloaded) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write('Downloaded ' + downloaded.toString() + '/' + totalNumberOfPages.toString());
+    if(downloaded === totalNumberOfPages) process.stdout.write('\n');
+});
 var mcgComic = null;
 var pathToMobi = null;
 
@@ -33,6 +49,17 @@ co(function *(){
     try{
         mcgComic = yield dm5Downloader.getPageImagesAsync();
         const opfBuilder = new OPFBuilder(mcgComic.title, mcgComic.imagePaths);
+        opfBuilder.on(OPFBuilderEvents.GenerateMobi, function() {
+            process.stdout.write('Start generating .mobi\n');
+        });
+        opfBuilder.on(OPFBuilderEvents.Processed, function(totalProcessed) {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            process.stdout.write('Resized ' + totalProcessed.toString() + '/' + totalNumberOfPages.toString());
+            if(totalProcessed === totalNumberOfPages) {
+                process.stdout.write('\n');
+            }
+        });
         pathToMobi = yield opfBuilder.buildBookAsync(options.imageQuality);
     } finally {
         if(mcgComic !== null) {
@@ -45,7 +72,6 @@ co(function *(){
         }
     }
 }).then(function(status) {
-    console.log('done');
     console.log('generated comic located at:');
     console.log(pathToMobi);
 }).catch(function(err) {
